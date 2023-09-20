e985d056b86b6970113fdce088ffa51b07ee295b
Luke Hartley <luke.hartley05@gmail.com>
2023-09-20 17:16:55 +0100

    Corrected typehint

--- primesieve/__init__.pyi
+++ primesieve/__init__.pyi
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.typing as npt
 
-def sieve(limit: int) -> npt.NDArray[np.int32]:
+def sieve(limit: int) -> npt.NDArray[np.int64]:
     """Return all prime numbers up to a given limit using Sieve of Eratosthenes."""
     ...
